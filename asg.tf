3aadf9432e80a604856f8cd82d1c537315788adb
abdul said <abdul-said@outlook.com>
2024-11-14 21:57:45 +0000

    asg update

---
+++ asg.tf
@@ -0,0 +1,47 @@
+resource "aws_launch_template" "auto-scaling-group-web" {
+  name = "auto-scaling-group-web"
+  image_id = data.aws_ami.myimage.image_id
+  instance_type = "t2.micro"
+  user_data = file("../user-data")
+  key_name = "terraform-key"
+  network_interfaces {
+    subnet_id = module.vpc.public_subnet_id_1
+    security_groups = [ aws_security_group.webserver_security_group ]
+  }
+}
+
+resource "aws_autoscaling_group" "asg-1" {
+  availability_zones = [ "eu-west-1" ]
+  desired_capacity = 1
+  max_size = 2
+  min_size = 1
+
+  launch_template {
+    id = aws_launch_template.auto-scaling-group-web.id
+    version = "$Latest"
+  }
+}
+
+resource "aws_launch_template" "auto-scaling-group-private" {
+  name = "auto-scaling-group-private"
+  image_id = data.aws_ami.myimage.image_id
+  instance_type = "t2.micro"
+  user_data = file("../user-data")
+  key_name = "terraform-key"
+  network_interfaces {
+    subnet_id = module.vpc.private_subnet_1_id
+    security_groups = [ aws_security_group.ssh_security_group ]
+  }
+}
+
+resource "aws_autoscaling_group" "asg-2" {
+  availability_zones = [ "eu-west-1" ]
+  desired_capacity = 1
+  max_size = 2
+  min_size = 1
+
+  launch_template {
+    id = aws_launch_template.auto-scaling-group-private.id
+    version = "$Latest"
+  }
+}
